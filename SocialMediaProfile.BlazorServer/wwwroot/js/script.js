7bae78550adcde555dec2beb0797c8544c9d33ac
Nico Ledesma <dev.ledesmanicolas@gmail.com>
2024-02-28 00:22:06 -0300

    Refactorización HTML User

--- SocialMediaProfile.BlazorServer/wwwroot/js/script.js
+++ SocialMediaProfile.BlazorServer/wwwroot/js/script.js
@@ -2,3 +2,14 @@
     var section = document.getElementById(sectionId);
     section.scrollIntoView({ behavior: 'smooth' });
 }
+function ScrollToAboutMeSection() {
+    var nav = document.getElementById("nav");
+    var banner = document.getElementById("banner");
+
+    var navHeight = nav.scrollHeight;
+    var bannerHeight = banner.scrollHeight;
+
+    var height = bannerHeight - navHeight - 145;
+
+    window.scrollTo({ top: height, behavior: 'smooth' });
+}
